4b420fbee3c9e6434abd0bcf21c10b1fa2f93761
Hugo Fitipaldi <33068886+hugofitipaldi@users.noreply.github.com>
2023-06-15 13:45:01 +0200

    few improvements in the app structure

--- scripts/app.py
+++ scripts/app.py
@@ -9,31 +9,66 @@ import pandas as pd
 df = pd.read_csv('../data/exercise_log.csv')
 tracker = ExerciseTracker(df)
 
+exercise_options = [
+    {'label': 'Front Squat', 'value': 'Front Squat'},
+    {'label': 'Snatch', 'value': 'Snatch'},
+    {'label': 'Back Squat', 'value': 'Back Squat'},
+    {'label': 'Power Snatch', 'value': 'Power Snatch'},
+    {'label': 'Deadlift', 'value': 'Deadlift'},
+    {'label': 'Power Clean', 'value': 'Power Clean'},
+    {'label': 'Clean and Jerk', 'value': 'Clean and Jerk'},
+    {'label': 'Push Jerk', 'value': 'Push Jerk'},
+    {'label': 'Split Jerk', 'value': 'Split Jerk'},
+    {'label': 'Bench Press', 'value': 'Bench Press'},
+    {'label': 'Hang Snatch', 'value': 'Hang Snatch'},
+    {'label': 'Overhead Squat', 'value': 'Overhead Squat'},
+    {'label': 'Push Press', 'value': 'Push Press'},
+    {'label': 'Hang Clean', 'value': 'Hang Clean'},
+    {'label': 'Clean', 'value': 'Clean'},
+    {'label': 'Strict Press', 'value': 'Strict Press'}
+]
+
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1('Exercise Tracker'),
-    dcc.Input(id='exercise-name', type='text', placeholder='Enter exercise name'),
-    dcc.Input(id='weight', type='number', placeholder='Enter weight'),
-    dcc.Input(id='reps', type='number', placeholder='Enter reps'),
-    dcc.Input(id='date', type='text', placeholder='Enter date (YYYY-MM-DD)'),
-    html.Button('Submit', id='submit', n_clicks=0),
-    html.Div(id='output'),
-    dcc.Graph(id='graph', figure={}),  # empty graph
-    html.Button('Save Data', id='save-button', n_clicks=0),  # save data button
-    html.Div(id='save-output')  # output for the save data button
+    dcc.Tabs([
+        dcc.Tab(label='Log Exercise', children=[
+            html.Div([
+                dcc.Dropdown(id='exercise-name', options=exercise_options, placeholder='Select exercise'),
+                dcc.Input(id='weight', type='number', placeholder='Enter weight'),
+                dcc.Input(id='reps', type='number', placeholder='Enter reps'),
+                dcc.Input(id='date', type='text', placeholder='Enter date (YYYY-MM-DD)'),
+                html.Button('Submit', id='submit', n_clicks=0),
+                html.Div(id='output'),
+                dcc.Graph(id='log-graph')
+            ]),
+            html.Button('Save Data', id='save-button', n_clicks=0),  # save data button
+            html.Div(id='save-output')  # output for the save data button
+        ]),
+        dcc.Tab(label='Predicted 1RM', children=[
+            html.Div([
+                dcc.Dropdown(id='exercise-prediction', options=exercise_options, placeholder='Select exercise'),
+                html.Button('Create Plot', id='plot-button', n_clicks=0),  # create plot button
+                dcc.Graph(id='prediction-graph')
+            ])
+        ])
+    ])
 ])
 
+# Log Exercise callback
 @app.callback(
     Output('output', 'children'),
-    Output('graph', 'figure'),
+    Output('log-graph', 'figure'),
     Input('submit', 'n_clicks'),
     State('exercise-name', 'value'),
     State('weight', 'value'),
     State('reps', 'value'),
     State('date', 'value'),
 )
-def update_output(n_clicks, exercise_name, weight, reps, date):
+
+def log_exercise(n_clicks, exercise_name, weight, reps, date):
     if n_clicks > 0:
         tracker.log_exercise(exercise_name, weight, reps, date)
         estimated_1rm = tracker.calculate_1rm(exercise_name)
@@ -41,21 +76,30 @@ def update_output(n_clicks, exercise_name, weight, reps, date):
         return f"The estimated 1RM for {exercise_name} is {estimated_1rm} kg", figure
     return dash.no_update, dash.no_update
 
+# Save Data callback
 @app.callback(
     Output('save-output', 'children'),
-    Input('save-button', 'n_clicks'),
+    Input('save-button', 'n_clicks')
 )
-
-def save_data(n):
-    if n > 0:  # if button is clicked
-        # Button has been clicked, save the data
+def save_data(n_clicks):
+    if n_clicks > 0:
         tracker.df.to_csv('../data/exercise_log.csv', index=False)
         return "Data Saved!"
-    return ""  # no message if button has not been clicked
+    return ""
 
+# Predicted 1RM callback
+@app.callback(
+    Output('prediction-graph', 'figure'),
+    Input('plot-button', 'n_clicks'),
+    State('exercise-prediction', 'value')
+)
+def plot_prediction(n_clicks, exercise_name):
+    if n_clicks > 0:
+        figure = tracker.plot_1rm(exercise_name)
+        return figure
+    return dash.no_update
 
 if __name__ == '__main__':
     # Open a new browser tab automatically when the script is run
     webbrowser.open_new_tab('http://localhost:8050')
     app.run_server(debug=False, host='127.0.0.1', port=8050)
-
